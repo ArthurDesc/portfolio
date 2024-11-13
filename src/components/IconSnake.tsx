9a91192a941dde7baec50782394c158b0dc9e413
Derroce <arthur.descourvieres@gmail.com>
2024-11-13 16:24:21 +0100

    first anim is cool

--- src/components/IconSnake.tsx
+++ src/components/IconSnake.tsx
@@ -8,24 +8,39 @@ interface IconProps {
 const basicSnakeVariants: Variants = {
   initial: { x: "200vw", y: "50vh" },
   animate: (index) => ({
-    x: ["200vw", "150vw", "100vw", "50vw", "0vw", "-50vw", "-100vw", "-150vw", "-200vw"],
+    x: [
+      "200vw", "180vw", "160vw", "140vw", "120vw", "100vw", "80vw", "60vw", "40vw", "20vw",
+      "0vw", "-20vw", "-40vw", "-60vw", "-80vw", "-100vw", "-120vw", "-140vw", "-160vw", "-180vw", "-200vw"
+    ],
     y: [
-      "50vh",    // Départ au milieu
-      "10vh",    // Plus haut
-      "65vh",    // Moins bas
-      "5vh",     // Encore plus haut
-      "60vh",    // Moins bas
-      "10vh",    // Plus haut
-      "65vh",    // Moins bas
-      "5vh",     // Encore plus haut
-      "50vh",    // Retour au milieu
+      "50vh",  // Départ centre
+      "35vh",  // Monte progressivement
+      "20vh",  // Continue de monter
+      "15vh",  // Presque au plus haut
+      "20vh",  // Redescend légèrement
+      "35vh",  // Continue de descendre
+      "45vh",  // Descend encore
+      "50vh",  // Point bas
+      "45vh",  // Remonte légèrement
+      "35vh",  // Continue de monter
+      "20vh",  // Monte encore
+      "15vh",  // Point haut
+      "20vh",  // Redescend légèrement
+      "35vh",  // Continue de descendre
+      "45vh",  // Descend encore
+      "50vh",  // Point bas
+      "45vh",  // Remonte légèrement
+      "35vh",  // Continue de monter
+      "20vh",  // Monte encore
+      "35vh",  // Redescend doucement
+      "50vh",  // Retour centre
     ],
     transition: {
       duration: 25,
       repeat: Infinity,
       ease: "linear",
       delay: index * 0.8,
-      times: [0, 0.125, 0.25, 0.375, 0.5, 0.625, 0.75, 0.875, 1],
+      times: [0, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95, 1],
       immediateStart: true,
     }
   })
